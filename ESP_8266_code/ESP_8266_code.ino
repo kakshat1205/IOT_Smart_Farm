93ebd801022d6c192b05ae54b81071717a7a63d4
Baba_Yaga <49076977+kakshat1205@users.noreply.github.com>
2021-04-30 04:03:21 +0530

    Add files via upload

--- ESP_8266_code/ESP_8266_code.ino
+++ ESP_8266_code/ESP_8266_code.ino
@@ -9,16 +9,34 @@ const int mqttPort = 12342;
 const char* mqttUser = "kakshat1205";
 const char* mqttPassword = "kakshat1205";
 
-const char *topic2 = "temp";
 const char *topic = "humidity";
+const char *topic2 = "temp";
 const char *topic3 = "motor";
+const char *topic4 = "LED_RED";
+const char *topic5 = "LED_BLUE";
+const char *topic6 = "LED_GREEN";
+
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
-
+unsigned long previousMillis = 0;
+int ledState = LOW;
+const long interval = 1000;
+/*
+  ICACHE_RAM_ATTR void detectsMovement() {
+  Serial.println("MOTION DETECTED!!!");
+  digitalWrite(led, HIGH);
+  startTimer = true;
+  lastTrigger = millis();
+  }
+*/
 void setup() {
   pinMode(D4, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D0 , OUTPUT);
+  //attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
+
   Serial.begin(115200);
   Wire.begin(D1, D2);
   WiFi.begin(ssid, password);
@@ -37,16 +55,27 @@ void setup() {
 
       Serial.print("failed with state ");
       Serial.print(client.state());
-      delay(2000);
+      delay(1000);
     }
   }
-
-  client.publish(topic, "humidity emqx");
+  digitalWrite(D4, HIGH);
+  digitalWrite(D0, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D7, HIGH);
   client.subscribe(topic);
-  client.publish(topic2, "temp emqx");
+  client.publish(topic, "hum");
   client.subscribe(topic2);
-  client.publish(topic3, "motor emqx");
+  client.publish(topic2, "tem");
+  //client.subscribe(topic2);
   client.subscribe(topic3);
+  client.publish(topic3, "on");
+  client.subscribe(topic4);
+  client.publish(topic4, "on");
+  client.subscribe(topic5);
+  client.publish(topic5, "on");
+  client.subscribe(topic6);
+  client.publish(topic6, "on");
+
 }
 
 void reconnect() {
@@ -64,30 +93,61 @@ void reconnect() {
   }
 }
 
+
 void loop() {
 
   client.loop();
-  Wire.beginTransmission(8); /* begin with device address 8 */
-  Wire.write("Hello Arduino");  /* sends hello string */
-  Wire.endTransmission();    /* stop transmitting */
+  unsigned long currentMillis = millis();
 
-  Wire.requestFrom(8, 1); /* request & read data of size 13 from slave */
-  while (Wire.available()) {
-    int c = Wire.read();
-    char ptr[10];
-    char str[10];
-    //Serial.print(c);
-    itoa(c, ptr, 10);
-    //Serial.println(ptr);
-    client.publish(topic, ptr);
-    //Serial.println(s);
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      Wire.beginTransmission(8); /* begin with device address 8 */
+      //Wire.write("Hello Arduino");  /* sends hello string */
+      //Wire.endTransmission();    /* stop transmitting */
+
+      Wire.requestFrom(8, 1); /* request & read data of size 13 from slave */
+      while (Wire.available()) {
+        int c = Wire.read();
+        /*if(c <= 25)
+          digitalWrite(D4, LOW);
+          else
+          digitalWrite(D4, HIGH);
+        */
+        int c2 = 34;
+        char ptr2[10];
+        char str[10];
+        char ptr[10];
+        char str2[10];
+        //Serial.print(c);
+
+        itoa(c, ptr, 10);
+        itoa(c2, ptr2, 10);
+        //Serial.println(ptr);
+        client.publish(topic, ptr);
+        client.publish(topic2, ptr2);
+        //Serial.println(s);
+        ledState = HIGH;
+      }
+    }
+    else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    //digitalWrite(ledPin, ledState);
   }
-  Serial.println();
-  delay(1000);
-
 
 
 }
+//Serial.println();
+//delay(1000);
+
+
+
 
 /*
   void callback(char* topic, byte* payload, unsigned int length) {
@@ -105,7 +165,7 @@ void loop() {
   }
 */
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte * payload, unsigned int length) {
   String response;
 
   for (int i = 0; i < length; i++) {
@@ -115,17 +175,77 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(topic);
   Serial.print("] ");
   Serial.println(response);
-  Serial.println("HI");
+  //Serial.println("HI");
   //digitalWrite(D4, LOW);
-  if (response == "on") // Turn the light on
-  {
-    digitalWrite(D4, HIGH);
-    //client.publish(STATE_TOPIC, "on");
-  }
-  else if (response == "off") // Turn the light off
-  {
-    digitalWrite(D4, LOW);
-    //client.publish(STATE_TOPIC, "off");
+  /*  switch (topic) {
+      case "motor":
+        D4 = D4;
+        break;
 
+      case "LED_RED":
+        D4 = D8;
+        break;
+
+      case "LED_GREEN":
+        D4 = D7;
+        break;
+
+      case "LED_BLUE":
+        D4 = D6;
+        break;
+
+    }
+  */
+  if (strcmp(topic, "motor") == 0)
+  {
+    if (response == "on") // Turn the light on
+    {
+      digitalWrite(D4, HIGH);
+      //client.publish(STATE_TOPIC, "on");
+    }
+    else if (response == "off") // Turn the light off
+    {
+      digitalWrite(D4, LOW);
+    }
+  }
+  else if (strcmp(topic, "LED_RED") == 0)
+  {
+    if (response == "on") // Turn the light on
+    {
+      Serial.println("RED");
+      digitalWrite(D3, HIGH);
+      //client.publish(STATE_TOPIC, "on");
+    }
+    else if (response == "off") // Turn the light off
+    {
+      digitalWrite(D3, LOW);
+    }
+  }
+  else if (strcmp(topic, "LED_BLUE") == 0)
+  {
+    if (response == "on") // Turn the light on
+    {
+
+      Serial.println("BLUE");
+      digitalWrite(D7, HIGH);
+      //client.publish(STATE_TOPIC, "on");
+    }
+    else if (response == "off") // Turn the light off
+    {
+      digitalWrite(D7, LOW);
+    }
+  }
+  else if (strcmp(topic, "LED_GREEN") == 0)
+  {
+    if (response == "on") // Turn the light on
+    {
+      Serial.println("GREEN");
+      digitalWrite(D0, HIGH);
+      //client.publish(STATE_TOPIC, "on");
+    }
+    else if (response == "off") // Turn the light off
+    {
+      digitalWrite(D0, LOW);
+    }
   }
 }
